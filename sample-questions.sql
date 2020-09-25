3ed4a051fd66c0cdeb0d58194ea5cccb559fba5f
Matthew Urrea <matt.urrea.code@gmail.com>
2020-09-25 13:14:43 -0600

    sample-questions.sql and seed data added

---
+++ sample-questions.sql
@@ -0,0 +1,9 @@
+https://www.techbeamers.com/sql-query-questions-answers-for-practice/#sql-scripts
+
+Q-1. Write an SQL query to fetch “FIRST_NAME” from Worker table using the alias name as <WORKER_NAME>.
+
+Q-2. Write an SQL query to fetch “FIRST_NAME” from Worker table in upper case.
+
+Q-3. Write an SQL query to fetch unique values of DEPARTMENT from Worker table.
+
+Q-4. Write an SQL query to print the first three characters of  FIRST_NAME from Worker table.
